a2af3a69153fbbbc3a5f85eec5ef83b0ca0d9c68
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-04-02 20:22:27 -0300

    teste

---
+++ criar-conta.php
@@ -0,0 +1,32 @@
+<?php
+    include('conexao.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar Conta</title>
+</head>
+<body>
+    <h1>Crie sua conta</h1>
+    <form action="criarConta.php" method="POST">
+        <p>
+            <label>Name</label>
+            <input type="text" name="name">
+        </p>
+        <p>
+            <label>E-mail</label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label>Senha</label>
+            <input type="password" name="pass">
+        </p>
+        <p>
+            <button type="submit">Criar Conta</button>
+        </p>
+    </form>
+</body>
+</html>
--- criarConta.php
+++ criarConta.php
@@ -11,35 +11,6 @@
 
     $query_users = mysqli_query($conexao, $criar_conta);
 
-    header('location:login.php')
+    header('location:login.php');
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Criar Conta</title>
-</head>
-<body>
-    <h1>Crie sua conta</h1>
-    <form action="" method="POST">
-        <p>
-            <label>Name</label>
-            <input type="text" name="name">
-        </p>
-        <p>
-            <label>E-mail</label>
-            <input type="text" name="email">
-        </p>
-        <p>
-            <label>Senha</label>
-            <input type="password" name="pass">
-        </p>
-        <p>
-            <button type="submit">Criar Conta</button>
-        </p>
-    </form>
-</body>
-</html>

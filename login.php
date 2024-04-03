8906413be992a6ba7219ee315b18bbef4f981fc1
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-04-02 21:29:15 -0300

    css - login

--- login.php
+++ login.php
@@ -7,22 +7,78 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app/styles/login.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Acesse sua conta</h1>
-    <form action="app/functions/session/verificarUser.php" method="POST">
-        <p>
-            <label>E-mail</label>
-            <input type="text" name="email">
-        </p>
-        <p>
-            <label>Senha</label>
-            <input type="password" name="pass">
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice">
+		<defs>
+			<linearGradient id="bg">
+				<stop offset="0%" style="stop-color:rgba(255, 136, 0, 0.990)"></stop>
+				<stop offset="50%" style="stop-color:rgba(205, 107, 0, 0.990)"></stop>
+				<stop offset="100%" style="stop-color:rgba(160, 73, 0, 0.990)"></stop>
+			</linearGradient>
+			<path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+	s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z" />
+		</defs>
+		<g>
+			<use xlink:href='#wave' opacity=".3">
+				<animateTransform
+                    attributeName="transform"
+                    attributeType="XML"
+                    type="translate"
+                    dur="10s"
+                    calcMode="spline"
+                    values="270 230; -334 180; 270 230"
+                    keyTimes="0; .5; 1"
+                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                    repeatCount="indefinite" />
+			</use>
+			<use xlink:href='#wave' opacity=".6">
+				<animateTransform
+                    attributeName="transform"
+                    attributeType="XML"
+                    type="translate"
+                    dur="8s"
+                    calcMode="spline"
+                    values="-270 230;243 220;-270 230"
+                    keyTimes="0; .6; 1"
+                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                    repeatCount="indefinite" />
+			</use>
+			<use xlink:href='#wave' opacty=".9">
+				<animateTransform
+                    attributeName="transform"
+                    attributeType="XML"
+                    type="translate"
+                    dur="6s"
+                    calcMode="spline"
+                    values="0 230;-140 200;0 230"
+                    keyTimes="0; .4; 1"
+                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                    repeatCount="indefinite" />
+			</use>
+		</g>
+	</svg>
+
+    <div class="container">
+        <h1>Login</h1>
+        <form action="app/functions/session/verificarUser.php" method="POST">
+            <p>
+                <label>E-mail</label>
+                <input type="text" name="email">
+            </p>
+            <p>
+                <label>Senha</label>
+                <input type="password" name="pass">
+            </p>
+            <p>
+                <button type="submit">Entrar</button>
+            </p>
+        </form>
+    </div>
+
+
 </body>
 </html>

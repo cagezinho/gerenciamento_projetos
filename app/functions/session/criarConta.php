11f359cd02fc97899c579ce86868a1911369048c
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-04-04 22:48:52 -0300

    Update criarConta.php

--- app/functions/session/criarConta.php
+++ app/functions/session/criarConta.php
@@ -2,15 +2,20 @@
     include('../conexao/conexao.php');
 
     if(strlen($_POST['name']) == 0 || strlen($_POST['email']) == 0 || strlen($_POST['pass']) == 0) {
+
         header('location:https://projetos.nicolascage.dev.br/app/pages/criar-conta.php');
+
     } else {
+
         $name = $_POST['name'];
         $email = $_POST['email'];
         $pass = $_POST['pass'];
+        
+        $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
     
         $criar_conta = "INSERT INTO
         db_users
-        VALUE ('', '$name', '$email', '$pass')";
+        VALUE ('', '$name', '$email', '$pass_hash')";
     
         $query_users = mysqli_query($conexao, $criar_conta);
     

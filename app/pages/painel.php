082b26c06892d718353f50f1ccb2019fc6c3e5a1
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-04-06 13:07:19 -0300

    update painel

--- app/pages/painel.php
+++ app/pages/painel.php
@@ -20,6 +20,17 @@
         <a class="logout" href="../functions/session/logout.php">Sair</a>
     </div>
 
+    <main>
+        <div class="header">
+            <!-- minimizar sidebar -->
+            <!-- profile -->
+        </div>
+        <div class="container">
+
+        </div>
+    </main>
+    
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>

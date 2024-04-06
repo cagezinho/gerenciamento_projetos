4f5d97a0a36ac0767af60c5818bc9eaf2ec439f2
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-04-06 13:20:23 -0300

    update

--- app/pages/painel.php
+++ app/pages/painel.php
@@ -13,14 +13,14 @@
 </head>
 <body>
 
-    <div class="sidebar">
-        <div class="profile">
-            <p href="#">Bem-vindo ao painel, <?php echo $_SESSION['name']; ?>.</p>
-        </div>
-        <a class="logout" href="../functions/session/logout.php">Sair</a>
-    </div>
-
     <main>
+        <div class="sidebar">
+            <div class="profile">
+                <p href="#">Bem-vindo ao painel, <?php echo $_SESSION['name']; ?>.</p>
+            </div>
+            <a class="logout" href="../functions/session/logout.php">Sair</a>
+        </div>
+
         <div class="header">
             <!-- minimizar sidebar -->
             <!-- profile -->
@@ -29,7 +29,6 @@
 
         </div>
     </main>
-    
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
